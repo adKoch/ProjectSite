89d2e92ac77ab391bdabbf97ea2ac454c462b0d0
adKoch <adam.kochanski.97@gmail.com>
2020-03-16 17:53:35 +0100

    init

--- project_site_frontend/src/core/MediaSiteLinks.ts
+++
@@ -1,7 +0,0 @@
-const links = {
-  github : 'https://github.com/adKoch',
-  linkedin : 'https://www.linkedin.com/in/adamkochanski97/',
-  facebook : 'https://www.facebook.com/adam.kochanski.399',
-};
-
-export default links;
