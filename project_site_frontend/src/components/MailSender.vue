43ff2b960c1b759c5c4eeeccb01526a103689e34
adKoch <adam.kochanski.97@gmail.com>
2020-03-18 12:57:04 +0100

    Fix Contact Captcha and gdpr consent

--- project_site_frontend/src/components/MailSender.vue
+++ project_site_frontend/src/components/MailSender.vue
@@ -1,14 +1,18 @@
 <template>
     <div>
         <v-row align="center" justify="center">
-            <vue-recaptcha sitekey="6LebfuAUAAAAAJI5unWoZ4IZnvMP9bqi5cSyS6Iw" class="justify-center"/>
+            <v-checkbox v-model="consentAgreed" :label="$t('gdprAgreement')"/>
+        </v-row>
+        <v-row align="center" justify="center">
+            <vue-recaptcha @verify="(response) => recaptchaResponse = response"
+                           sitekey="6LebfuAUAAAAAJI5unWoZ4IZnvMP9bqi5cSyS6Iw" class="justify-center"/>
         </v-row>
         <v-btn rounded
                class="ma-5"
-               :disabled="title==='' || content==='' || sign===''"
-               @click="sendMail(title, content, sign,
-               () => {successSnackbarVisibility = true},
-               () => {errorSnackbarVisibility = true})">
+               :disabled="title==='' || content==='' || sign==='' || recaptchaResponse==='' || !consentAgreed"
+               @click="sendMail(title, content, sign, recaptchaResponse,
+               () => successSnackbarVisibility = true,
+               () => errorSnackbarVisibility = true)">
             {{$t('sendButtonLabel')}}
         </v-btn>
         <v-snackbar v-model="successSnackbarVisibility">
@@ -22,8 +26,8 @@
         <v-snackbar v-model="errorSnackbarVisibility">
             {{$t('errorOnSent')}}
             <v-btn color="primary"
-                    text
-                    @click="errorSnackbarVisibility = false">
+                   text
+                   @click="errorSnackbarVisibility = false">
                 {{$t('closeSnackbar')}}
             </v-btn>
         </v-snackbar>
@@ -32,26 +36,28 @@
 
 <script lang="ts">
 
-import VueRecaptcha from 'vue-recaptcha';
-import {sendMail, initMail} from '@/core/EmailSending';
+    import VueRecaptcha from 'vue-recaptcha';
+    import {sendMail, initMail} from '@/core/EmailSending';
 
-export default {
-  name: 'MailSender',
-  props: ['title', 'content', 'sign'],
-  components: {
-    VueRecaptcha,
-  },
-  data: () => ({
-    successSnackbarVisibility: false,
-    errorSnackbarVisibility: false,
-  }),
-  methods: {
-    sendMail,
-  },
-  mounted(): void {
-    initMail();
-  },
-};
+    export default {
+        name: 'MailSender',
+        props: ['title', 'content', 'sign'],
+        components: {
+            VueRecaptcha,
+        },
+        data: () => ({
+            successSnackbarVisibility: false,
+            errorSnackbarVisibility: false,
+            recaptchaResponse: '',
+            consentAgreed: false,
+        }),
+        methods: {
+            sendMail,
+        },
+        mounted(): void {
+            initMail();
+        },
+    };
 </script>
 
 <i18n>
@@ -60,13 +66,15 @@ export default {
     "sendButtonLabel":"Send",
     "successfullySent":"Successfully sent!",
     "errorOnSent":"Error sending your message. Try again later...",
-    "closeSnackbar":"Close"
+    "closeSnackbar":"Close",
+    "gdprAgreement":"I consent to having my information processed in order to respond to my inquiry."
     },
     "pl": {
     "sendButtonLabel":"Wyślij",
     "successfullySent":"Pomyślnie wysłano!",
     "errorOnSent":"Wystąpił problem podczas wysyłania twojej wiadomości. Spróbuj ponownie później...",
-    "closeSnackbar":"Zamknij"
+    "closeSnackbar":"Zamknij",
+    "gdprAgreement":"Wyrażam zgodę na przetwarzanie danych zawartych w wypełnionych formularzach w celu możliwości udzielenia odpowiedzi na wysłaną wiadomość."
     }
     }
 </i18n>
--- project_site_frontend/src/core/EmailSending.ts
+++ project_site_frontend/src/core/EmailSending.ts
@@ -11,12 +11,14 @@ export function initMail() {
 export function sendMail(title: string,
                          content: string,
                          sign: string,
+                         recaptchaResponse: string,
                          onSuccess: (status: number, text: string) => any,
                          onError: (error: any) => any) {
   const templateParams = {
-    message_title: title,
-    message_content: content,
-    message_sign: sign,
+    'message_title': title,
+    'message_content': content,
+    'message_sign': sign,
+    'g-recaptcha-response': recaptchaResponse,
   };
   return emailjs.send(serviceId, templateId, templateParams)
     .then((result) => onSuccess(result.status, result.text), (error) => onError(error));
